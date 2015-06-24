4dddd1f67ff5881b36f7dc104acb163c2d877023
trungjc <trungnt@exoplatform.com>
2015-06-24 10:16:45 +0700

    upgrade lock down

--- wp-config.php
+++ wp-config.php
@@ -32,6 +32,8 @@ define('DB_CHARSET', 'utf8mb4');
 
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
+/*define( 'DISALLOW_FILE_EDIT', true);
+define('DISALLOW_FILE_MODS',true);*/
 
 /**#@+
  * Authentication Unique Keys and Salts.
